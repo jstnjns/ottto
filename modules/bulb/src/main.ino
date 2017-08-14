1d440d498e8acb2d1dff3c5a095dff67c760b36c
Justin Jones <justin@jstnjns.com>
2017-08-14 00:55:17 -0700

    Updating bulb module with Ottto class

--- modules/bulb/src/main.ino
+++ modules/bulb/src/main.ino
@@ -1,11 +1,4 @@
-#include "functional"
-#include "Arduino.h"
-#include "ArduinoOTA.h"
-#include "ArduinoJson.h"
-#include "IPAddress.h"
-#include "ESP8266WiFi.h"
-#include "PubSubClient.h"
-#include "WiFiManager.h"
+#include "Ottto.h"
 
 #include "my9291.h"
 #include "RGBConverter.h"
@@ -13,28 +6,22 @@
 #define MY9291_DI_PIN 13
 #define MY9291_DCKI_PIN 15
 
-IPAddress serverAddress(10,10,0,1);
-const int mqttPort = 1883;
-const int httpPort = 1337;
-
-char moduleName[] = "lightbulb";
-char topic[] = "modules/16";
+otttoConfig config = {
+  .name = "lightbulb",
+  .topic = "modules/16",
+  .host = "10.10.0.1"
+};
+Ottto ottto(config);
 
 my9291 lights = my9291(MY9291_DI_PIN, MY9291_DCKI_PIN, MY9291_COMMAND_DEFAULT);
 RGBConverter colorConverter;
-WiFiManager wifiManager;
-WiFiClient espClient;
-PubSubClient client(espClient);
 
 
 void setup() {
   Serial.begin(115200);
 
-  // setupOTA();
-  // wifiManager.resetSettings();
-  wifiManager.autoConnect(moduleName);
-  client.setServer(serverAddress, mqttPort);
-  client.setCallback(receive);
+  ottto.begin();
+  ottto.subscribe(receive);
 
   setPower(false);
   delay(100);
@@ -44,49 +31,23 @@ void setup() {
 }
 
 
-void setupOTA() {
-  WiFi.mode(WIFI_STA);
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    Serial.println("Start updating...");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-}
-
-
 void receive(char* topic, uint8_t* payload, unsigned int length) {
-  Serial.print("Received: ");
-  Serial.print(topic);
-  Serial.print(": ");
-
-  // Build message buffer
   char message[length + 1];
   for (int i = 0; i < length; i++) {
     message[i] = (char)payload[i];
   }
   message[length] = '\0';
+
+  Serial.print("Received: ");
+  Serial.print(topic);
+  Serial.print(": ");
   Serial.println(message);
 
-  processJson(message);
+  process(message);
 }
 
 
-void processJson(char* message) {
+void process(char* message) {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& body = jsonBuffer.parseObject(message);
 
@@ -116,34 +77,11 @@ void setPower(bool powerValue) {
 
 
 void loop() {
-  ArduinoOTA.handle();
-
-  if (!client.connected()) {
-    connect();
-  }
-
-  client.loop();
+  ottto.loop();
 }
 
 
-void connect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect(moduleName)) {
-      Serial.println("connected");
-      client.subscribe(topic);
-      publish();
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
-
-
-void publish() {
+char* payload() {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& json = jsonBuffer.createObject();
   JsonObject& values = json.createNestedObject("values");
@@ -154,9 +92,7 @@ void publish() {
   char buffer[json.measureLength() + 1];
   json.printTo(buffer, sizeof(buffer));
 
-  Serial.print("Sending: ");
-  Serial.println(buffer);
-  client.publish(topic, buffer);
+  return buffer;
 }
 
 
