bce63afefd2d5f55fcf7cf654134aa98bc6e02d1
Justin Jones <justin@jstnjns.com>
2017-10-24 02:29:29 -0700

    icons

--- client/src/modules/modules-list.js
+++ client/src/modules/modules-list.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router'
 import Grid from 'material-ui/Grid'
 import Paper from 'material-ui/Paper'
 import Typography from 'material-ui/Typography'
+import FiberManualRecord from 'material-ui-icons/FiberManualRecord'
 
 
 class ModulesList extends Component {
   render() {
     return (
-      <Grid container spacing={16}
-        style={{ padding: 16 }}>
+      <Grid style={{ padding: 16 }}
+        spacing={16}
+        justify="center"
+        container>
         {this.props.modules.map(this.renderRoom.bind(this))}
       </Grid>
     )
@@ -17,13 +20,17 @@ class ModulesList extends Component {
 
   renderRoom(module, key) {
     return (
-      <Grid item key={key} xs={3}>
-        <Link to={`/modules/${module.id}`}>
-          <Paper style={{ height: 80 }}>
-          </Paper>
-            <Typography type="caption">
-              {module.name}
-            </Typography>
+      <Grid item key={key} xs={3} sm={2}>
+        <Link to={`/modules/${module.id}`}
+          style={{ textDecoration: 'none', textAlign: 'center' }}>
+          <Typography align="center">
+            <FiberManualRecord style={{ fill: '#CCCCCC', width: 72, height: 72 }} />
+          </Typography>
+
+          <Typography type="caption"
+            align="center">
+            {module.name}
+          </Typography>
         </Link>
       </Grid>
     )
