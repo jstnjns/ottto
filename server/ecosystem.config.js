ed7f97da4f2c5511ec0760e82976eca4cde17174
Justin Jones <justin@jstnjns.com>
2017-11-11 13:51:22 -0800

    moar config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -17,15 +17,15 @@ module.exports = {
       user : 'pi',
       host : '192.168.1.5',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'git@github.com:jstnjns/ottto.git',
       path : '/var/www/ottto/server',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
       user : 'node',
-      host : '212.83.163.1',
+      host : 'localhost',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'git@github.com:jstnjns/ottto.git',
       path : '/Users/justin/Development/projects/ottto/server',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
       env  : {
